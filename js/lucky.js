aecb7ade9826218179c6ca534c55861ba9a187e0
Sujan Devkota <sujandevkota@Sujans-MacBook-Pro.local>
2017-07-23 07:01:52 +0545

    Updates by Sujan

--- js/lucky.js
+++ js/lucky.js
@@ -1,7 +1,9 @@
 var runLoop = false;
 var minNumber = 1;
-var maxNumber = 100;
+var maxNumber = 130;
 var intervalTime = 200;
+var toselect =2;
+var selectcount=0;
 var selectedVal = [];
 setInterval(function(){ 
 	if(runLoop){
@@ -9,7 +11,8 @@ setInterval(function(){
 		randomNumber = generateRandomNumberInRange();
 		document.getElementById("number-display").innerHTML = randomNumber;
 	}
-}, intervalTime);		
+}, intervalTime);
+
 function generateRandomNumberInRange(){
 	var num  = Math.floor(Math.random() * maxNumber) + minNumber;
 	if(selectedVal.indexOf(num) != -1){
@@ -19,18 +22,31 @@ function generateRandomNumberInRange(){
 	}
 	
 }
+
+
+
 window.onload = function(e){ 
 	document.getElementById("start-btn").addEventListener("click", function(){
 		runLoop = true;
+		document.getElementById("start-btn").style.display = "none"; 
 	});
 	document.getElementById("end-btn").addEventListener("click", function(){
-		runLoop = false;
-		var ul = document.getElementById("winner-list");
-	  	var li = document.createElement("li");
-	  	li.appendChild(document.createTextNode(document.getElementById("number-display").innerHTML));
-	  	ul.appendChild(li);			
+
+			
+			runLoop = false;
+			var ul = document.getElementById("winner-list");
+		  	var li = document.createElement("li");
+		  	li.appendChild(document.createTextNode(document.getElementById("number-display").innerHTML));
+		  	ul.appendChild(li);	
+		  	selectcount++;
+			runLoop = true;		
+			
+			if(selectcount >= toselect) {
+				runLoop = false;		
+				document.getElementById("number-display").innerHTML = "Thank You";
+				document.getElementById("btn-wrap").style.display = "none"; 
+			}
+		
 	});
-	document.getElementById("clear-btn").addEventListener("click", function(){
-		clearWinnerList();
-	});			
+	
 }
